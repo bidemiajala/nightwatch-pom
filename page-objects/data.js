879271d25bfa4718aaf5419d04289be65288521c
Bidemi Ajala <bidemiajala@gmail.com>
2020-02-05 22:52:00 +0100

    changed data variables to shorthand

--- page-objects/data.js
+++ page-objects/data.js
@@ -1,10 +1,11 @@
 //This contains all data needed for tests
 const productName = 'Oplee White Power Wireless';
 const quantity = '3';
+const baseURL= "https://andela.netface.co/";
 
 module.exports = {
-    baseURL: "https://andela.netface.co/",
-    productName: productName,
-    quantity: quantity,
+    baseURL,
+    productName,
+    quantity,
     addToCartSuccessMessage: `${quantity} × “${productName}” have been added to your cart.`,
 };
